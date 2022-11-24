839edec9a0a7e59fc55ab8393d8c831589c57287
Rafał Gałecki <darthunter@gmail.com>
2022-11-24 18:28:39 +0100

    task 1 and 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
+"use strict";
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listOfIngredients = document.querySelector("#ingredients");
+for (const ingredient of ingredients) {
+  const item = document.createElement("li");
+  item.classList.add("item");
+  item.textContent = ingredient;
+  listOfIngredients.append(item);
+}
