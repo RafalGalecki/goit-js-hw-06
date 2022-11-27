632e29e2b1ea484e03b5b3db62435440916c0769
Rafał Gałecki <darthunter@gmail.com>
2022-11-27 17:24:03 +0100

    Task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+"use strict";
+
+const btnChangeColor = document.querySelector("button.change-color");
+
+const body = document.querySelector("body");
+
+const spanHexColorValue = document.querySelector("span.color");
+
+btnChangeColor.addEventListener("click", getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  let bgColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  body.style.backgroundColor = bgColor;
+  spanHexColorValue.textContent = bgColor;
 }
