c9872a3c300a4fdfcb741782a0027b5f87c1574b
Rafał Gałecki <darthunter@gmail.com>
2022-11-27 16:52:29 +0100

    Task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+"use strict";
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+const userLoginData = {};
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  userLoginData.email = email.value;
+  userLoginData.password = password.value;
+  console.log(userLoginData);
+  event.currentTarget.reset();
+}
